788d0ec7738b48a9fe99d6830c7ae5d2bf101440
Johnny <1015715475@qq.com>
2021-05-30 19:21:28 +0800

    创建三个页面

---
+++ src/views/Money.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Money.vue
+    </div>
+</template>
+
+<script setup lang="ts">
+    export default {
+        name: 'Money'
+    }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>

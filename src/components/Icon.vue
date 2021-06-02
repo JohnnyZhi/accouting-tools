fda737f434d350f83cd6687977f419d1724b196e
Johnny <1015715475@qq.com>
2021-06-02 22:40:21 +0800

    Icon.vue中的script改为js

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -4,9 +4,9 @@
      </svg>
 </template>
 
-<script  lang="ts">
-    // const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-    // try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
+<script >
+    const importAll = (requireContext) => requireContext.keys().forEach(requireContext);
+    try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
     
     export default {
         props: ['name'],

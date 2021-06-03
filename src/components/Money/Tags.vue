11ffcf45968aec9f78d715aca45703fdae5a774f
Johnny <1015715475@qq.com>
2021-06-03 21:45:46 +0800

    添加tags组件

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,22 +1,44 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
-    <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+     <ul class="current">
+      <li v-for="tag in dataSource" :key="tag"
+          :class="{selected: selectedTags.indexOf(tag)>=0}"
+          @click="toggle(tag)">{{tag}}
+      </li>
     </ul>
   </div>
-
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Tags'
-  };
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+
+@Component
+  export default class Tags extends Vue {
+  @Prop(Array) readonly dataSource: string[] | undefined;
+    selectedTags: string[] = [];
+
+   toggle(tag: string) {
+      const index = this.selectedTags.indexOf(tag);
+      if (index >= 0) {
+        this.selectedTags.splice(index, 1);
+      } else {
+        this.selectedTags.push(tag);
+      }
+    }
+    create() {
+      const name = window.prompt('请输入标签名');
+      if (name === '') {
+        window.alert('标签名不能为空');
+      } else if (this.dataSource) {
+        this.$emit('update:dataSource',
+          [...this.dataSource, name]);
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
@@ -38,6 +60,9 @@
         padding: 0 16px;
         margin-right: 12px;
         margin-top: 4px;
+        &.selected{
+          background:rgb(178,138,87) ;
+        }
       }
     }
     > .new {
